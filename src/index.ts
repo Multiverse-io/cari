0cac7a8b5253d705766dc4c46d2ff9e90b089e68
Tim Gent <tim.gent@multiverse.io>
2025-03-18 21:50:11 +0000

    Rm pointless comments

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,13 @@ import { Command } from "commander";
 import { init } from "./commands/init.js";
 import { update } from "./commands/update.js";
 
-// Create a new Commander program
 const program = new Command();
 
-// Configure the CLI
 program
   .name("ari")
   .description("AI Rules Installer - A CLI tool for installing AI rules")
   .version("1.0.0");
 
-// Add init command
 program
   .command("init")
   .description("Initialize AI Rules Installer in the current directory")
@@ -25,7 +22,6 @@ program
     await init();
   });
 
-// Add update command
 program
   .command("update")
   .description("Update AI rules in the current directory")
@@ -33,5 +29,4 @@ program
     await update();
   });
 
-// Parse command line arguments
 program.parse();

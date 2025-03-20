5e2740bfa199795d0b28c49cc241dabb404d6064
Tim Gent <tim.gent@multiverse.io>
2025-03-20 08:48:21 +0000

    Split out prompting test file

--- src/prompting/common.test.ts
+++ src/prompting/common.test.ts
@@ -1,105 +1,11 @@
 import { describe, expect, it } from "vitest";
-import { getSelectRuleChoices } from "./init-prompts.js";
 import {
-  repoChoice,
   directoryChoice,
   fileChoice,
+  repoChoice,
   userChoicesToSelectedRules,
 } from "./common.js";
-
-describe("getSelectRuleChoices", () => {
-  it("should return a list of choices", () => {
-    const centralRules = [
-      {
-        org: "my-org",
-        repo: "my-repo",
-        relativeFilePaths: [
-          {
-            categoryFolderName: "typescript",
-            fileName: "rule-1.mdc",
-          },
-          {
-            categoryFolderName: "typescript",
-            fileName: "rule-2.mdc",
-          },
-        ],
-      },
-      {
-        org: "my-other-org",
-        repo: "my-other-repo",
-        relativeFilePaths: [
-          {
-            categoryFolderName: "elixir",
-            fileName: "rule-3.mdc",
-          },
-          {
-            categoryFolderName: "elixir",
-            fileName: "rule-4.mdc",
-          },
-        ],
-      },
-    ];
-    const choices = getSelectRuleChoices(centralRules);
-    expect(choices).toEqual([
-      repoChoice("my-org", "my-repo", [
-        {
-          categoryFolderName: "typescript",
-          fileName: "rule-1.mdc",
-        },
-        {
-          categoryFolderName: "typescript",
-          fileName: "rule-2.mdc",
-        },
-      ]),
-      directoryChoice("my-org", "my-repo", "typescript", [
-        {
-          categoryFolderName: "typescript",
-          fileName: "rule-1.mdc",
-        },
-        {
-          categoryFolderName: "typescript",
-          fileName: "rule-2.mdc",
-        },
-      ]),
-      fileChoice("my-org", "my-repo", {
-        categoryFolderName: "typescript",
-        fileName: "rule-1.mdc",
-      }),
-      fileChoice("my-org", "my-repo", {
-        categoryFolderName: "typescript",
-        fileName: "rule-2.mdc",
-      }),
-      repoChoice("my-other-org", "my-other-repo", [
-        {
-          categoryFolderName: "elixir",
-          fileName: "rule-3.mdc",
-        },
-        {
-          categoryFolderName: "elixir",
-          fileName: "rule-4.mdc",
-        },
-      ]),
-      directoryChoice("my-other-org", "my-other-repo", "elixir", [
-        {
-          categoryFolderName: "elixir",
-          fileName: "rule-3.mdc",
-        },
-        {
-          categoryFolderName: "elixir",
-          fileName: "rule-4.mdc",
-        },
-      ]),
-      fileChoice("my-other-org", "my-other-repo", {
-        categoryFolderName: "elixir",
-        fileName: "rule-3.mdc",
-      }),
-      fileChoice("my-other-org", "my-other-repo", {
-        categoryFolderName: "elixir",
-        fileName: "rule-4.mdc",
-      }),
-    ]);
-  });
-});
+import { FlatRepoRule, FlatSelectedRules } from "../rules/types.js";
 
 describe("userChoicesToSelectedRules", () => {
   const centralRules: FlatRepoRule[] = [

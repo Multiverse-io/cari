b5710bf6ebd86beedd86ee99e91e79f7b89199d3
Tim Gent <tim.gent@multiverse.io>
2025-03-20 08:54:08 +0000

    Rm unused custom groupBy fn

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,3 @@
-type Grouped<T> = { [key: string]: T[] };
-
-export function groupBy<T>(array: T[], key: keyof T): Grouped<T> {
-  return array.reduce((result, item) => {
-    const groupKey = String(item[key]);
-    (result[groupKey] = result[groupKey] || []).push(item);
-    return result;
-  }, {} as Grouped<T>);
-}
-
 export const mergeObjects = (
   ...objects: Record<string, any>[]
 ): Record<string, any> => {
